5f4add2d23f4f7425d135372c0acf8746bcc957d
lightingtea <davin0706@gmail.com>
2023-07-05 22:02:46 +0900

    task

--- 최소공배수(1934).c
+++ 최소공배수(1934).c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int gcd_count(int a, int b){
+int gcd(int a, int b){
 	while (b != 0){
 		int remain = a % b;
 		a = b;
@@ -11,13 +11,12 @@ int gcd_count(int a, int b){
 }
 
 int main(void){
-	int testcase, n1, n2, gcd, lcm;
+	int testcase, n1, n2, lcm;
 	scanf("%d", &testcase);
 	
 	for (int i = 0; i < testcase; i++){
 		scanf("%d %d", &n1, &n2);
-		gcd = gcd_count(n1, n2);
-		lcm = n1 * n2 / gcd;
+		lcm = n1 * n2 / gcd(n1, n2);
 		printf("%d\n", lcm);
 	}
 	
