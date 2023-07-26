cf567f13b39829ba4e6329560bb6ce9f534ac120
lightingtea <davin0706@gmail.com>
2023-07-26 23:46:36 +0900

    task

--- 4week/최소 힙(1927).py
+++ 4week/최소 힙(1927).py
@@ -1,3 +1,14 @@
-n = int(input())
-heap = [0 for i in range(n)]
+import heapq
+import sys
 
+N = int(sys.stdin.readline())
+h = []
+for _ in range(N):
+    x = int(sys.stdin.readline())
+    if x == 0:
+        if len(h) == 0:
+            print(0)
+        else:
+            print(heapq.heappop(h))
+    else:
+        heapq.heappush(h, x)
