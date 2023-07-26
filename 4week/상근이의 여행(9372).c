7054daaec5b3b1da13c219a0db30d3b64d092ff1
lightingtea <davin0706@gmail.com>
2023-07-26 23:06:14 +0900

    task

---
+++ 4week/상근이의 여행(9372).c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    int test, n, m, a, b;
+    scanf("%d", &test);
+
+    for (int i = 0; i < test; i++){
+        scanf("%d %d", &n, &m);
+        int* arr
+        for (int j = 0; j < m; j++){
+            scanf("%d %d", &a, &b);
+        }
+    }
+
+    return 0;
+}
