316506a583adf14a1e522d267027eca5cd0a56c9
tealight03 <davin0706@gmail.com>
2023-08-08 14:47:12 +0900

    6week tasks

---
+++ 6week/숫자 카드(10815).py
@@ -0,0 +1,26 @@
+def binary_search(key, card):
+    start, end = 0, len(card)
+    
+    while(start < end):
+        mid = (start + end) // 2
+
+        if card[mid] == key:
+            return 1
+        elif card[mid] < key:
+            start = mid+1
+        else:
+            end = mid
+        
+    return 0
+
+
+n = int(input())
+card = list(map(int, input().split()))
+card.sort()
+
+m = int(input())
+comp = list(map(int, input().split()))
+
+for data in comp:
+    flag = binary_search(data, card)
+    print(flag, end = " ")
